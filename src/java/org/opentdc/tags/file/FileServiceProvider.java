21cafd28a6f162af24210637266e0a4993a2544f
Bruno Kaiser <bruno@bkaiser.ch>
2015-07-13 01:51:02 +0200

    TagsService v1, all tests successful

--- src/java/org/opentdc/tags/file/FileServiceProvider.java
+++ src/java/org/opentdc/tags/file/FileServiceProvider.java
@@ -27,10 +27,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
@@ -38,24 +39,21 @@ import javax.servlet.ServletContext;
 import org.opentdc.file.AbstractFileServiceProvider;
 import org.opentdc.tags.TagsModel;
 import org.opentdc.tags.ServiceProvider;
+import org.opentdc.service.LocalizedTextModel;
 import org.opentdc.service.exception.DuplicateException;
 import org.opentdc.service.exception.InternalServerErrorException;
 import org.opentdc.service.exception.NotFoundException;
 import org.opentdc.service.exception.ValidationException;
 import org.opentdc.util.PrettyPrinter;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
 /**
  * A file-based or transient implementation of the Tags service.
  * @author Bruno Kaiser
  *
  */
-public class FileServiceProvider implements AbstractFileServiceProvider<TagsModel> implements ServiceProvider {
-	
-	private static Map<String, TagsModel> index = null;
+public class FileServiceProvider extends AbstractFileServiceProvider<TextedTag> implements ServiceProvider {
+	private static Map<String, TextedTag> index = null;
+	private static Map<String, LocalizedTextModel> textIndex = null;
 	private static final Logger logger = Logger.getLogger(FileServiceProvider.class.getName());
 
 	/**
@@ -69,27 +67,37 @@ public class FileServiceProvider implements AbstractFileServiceProvider<TagsMode
 		String prefix
 	) throws IOException {
 		super(context, prefix);
+		logger.info("tags-service.FileServiceProvider.Constructor()");
 		if (index == null) {
-			index = new HashMap<String, TagsModel>();
-			List<TagsModel> _tags = importJson();
-			for (TagsModel _tag : _tags) {
-				index.put(_tag.getId(), _tag);
+			index = new ConcurrentHashMap<String, TextedTag>();
+			textIndex = new ConcurrentHashMap<String, LocalizedTextModel>();
+			List<TextedTag> _tags = importJson();
+			for (TextedTag _tag : _tags) {
+				index.put(_tag.getModel().getId(), _tag);
+				for (LocalizedTextModel _tm : _tag.getLocalizedTexts()) {
+					textIndex.put(_tm.getId(), _tm);
+				}
 			}
-			logger.info(_tags.size() + " Tags imported.");
+			logger.info("indexed " +
+					index.size() + " tags and " +
+					textIndex.size() + " localized texts.");
 		}
 	}
 
 	/* (non-Javadoc)
-	 * @see org.opentdc.tags.ServiceProvider#list(java.lang.String, java.lang.String, long, long)
+	 * @see org.opentdc.tags.ServiceProvider#list(java.lang.String, java.lang.String, int, int)
 	 */
 	@Override
 	public ArrayList<TagsModel> list(
 		String queryType,
 		String query,
-		long position,
-		long size
+		int position,
+		int size
 	) {
-		ArrayList<TagsModel> _tags = new ArrayList<TagsModel>(index.values());
+		ArrayList<TagsModel> _tags = new ArrayList<TagsModel>();
+		for (TextedTag _tag : index.values()) {
+			_tags.add(_tag.getModel());
+		}
 		Collections.sort(_tags, TagsModel.TagComparator);
 		ArrayList<TagsModel> _selection = new ArrayList<TagsModel>();
 		for (int i = 0; i < _tags.size(); i++) {
@@ -109,38 +117,41 @@ public class FileServiceProvider implements AbstractFileServiceProvider<TagsMode
 	public TagsModel create(
 		TagsModel tag) 
 	throws DuplicateException, ValidationException {
+		TextedTag _textedTag = null;
 		logger.info("create(" + PrettyPrinter.prettyPrintAsJSON(tag) + ")");
 		String _id = tag.getId();
+		Date _date = new Date();
 		if (_id == null || _id == "") {
 			_id = UUID.randomUUID().toString();
+			tag.setId(_id);
+			tag.setCounter(1);
+			tag.setCreatedAt(_date);
+			tag.setCreatedBy(getPrincipal());
+			tag.setModifiedAt(_date);
+			tag.setModifiedBy(getPrincipal());
+			_textedTag = new TextedTag();
+			_textedTag.setModel(tag);
 		} else {
-			if (index.get(_id) != null) {
-				// object with same ID exists already
-				throw new DuplicateException("tag <" + _id + "> exists already.");
+			_textedTag = index.get(_id);
+			if (_textedTag != null) {
+				TagsModel _tm = _textedTag.getModel();
+				_tm.setCounter(_tm.getCounter() + 1);
+				_tm.setModifiedAt(_date);
+				_tm.setModifiedBy(getPrincipal());
+				_textedTag.setModel(_tm);
+				index.put(_id, _textedTag);
 			}
 			else { 	// a new ID was set on the client; we do not allow this
 				throw new ValidationException("tag <" + _id + 
 					"> contains an ID generated on the client. This is not allowed.");
 			}
 		}
-		// enforce mandatory fields
-		if (tag.getTitle() == null || tag.getTitle().length() == 0) {
-			throw new ValidationException("tag <" + _id + 
-					"> must contain a valid title.");
-		}
-
-		tag.setId(_id);
-		Date _date = new Date();
-		tag.setCreatedAt(_date);
-		tag.setCreatedBy(getPrincipal());
-		tag.setModifiedAt(_date);
-		tag.setModifiedBy(getPrincipal());
-		index.put(_id, tag);
-		logger.info("create(" + PrettyPrinter.prettyPrintAsJSON(tag) + ")");
+		index.put(_id,  _textedTag);
+		logger.info("create(" + PrettyPrinter.prettyPrintAsJSON(_textedTag.getModel()) + ")");
 		if (isPersistent) {
 			exportJson(index.values());
 		}
-		return tag;
+		return _textedTag.getModel();
 	}
 
 	/* (non-Javadoc)
@@ -150,13 +161,27 @@ public class FileServiceProvider implements AbstractFileServiceProvider<TagsMode
 	public TagsModel read(
 		String id) 
 	throws NotFoundException {
-		TagsModel _tag = index.get(id);
-		if (_tag == null) {
-			throw new NotFoundException("no tag with ID <" + id
-					+ "> was found.");
-		}
+		TagsModel _tag = readTextedTag(id).getModel();
 		logger.info("read(" + id + ") -> " + PrettyPrinter.prettyPrintAsJSON(_tag));
 		return _tag;
+	}
+	
+	/**
+	 * Retrieve a TextedTag from the index.
+	 * @param id
+	 * @return the TextedTag
+	 * @throws NotFoundException if the index did not contain a TextedTag with this id
+	 */
+	private TextedTag readTextedTag(
+			String id
+	) throws NotFoundException {
+		TextedTag _textedTag = index.get(id);
+		if (_textedTag == null) {
+			throw new NotFoundException("tag <" + id
+					+ "> was not found.");
+		}
+		logger.info("readTextedTag(" + id + ") -> " + PrettyPrinter.prettyPrintAsJSON(_textedTag));
+		return _textedTag;
 	}
 
 	/* (non-Javadoc)
@@ -167,29 +192,29 @@ public class FileServiceProvider implements AbstractFileServiceProvider<TagsMode
 		String id, 
 		TagsModel tag
 	) throws NotFoundException, ValidationException {
-		TagsModel _tag = index.get(id);
-		if(_tag == null) {
-			throw new NotFoundException("no tag with ID <" + id
-					+ "> was found.");
-		} 
-		if (! _tag.getCreatedAt().equals(tag.getCreatedAt())) {
+		TextedTag _textedTag = readTextedTag(id);
+		TagsModel _tagsModel = _textedTag.getModel();
+		if (! _tagsModel.getCreatedAt().equals(tag.getCreatedAt())) {
 			logger.warning("tag <" + id + ">: ignoring createdAt value <" + tag.getCreatedAt().toString() + 
 					"> because it was set on the client.");
 		}
-		if (! _tag.getCreatedBy().equalsIgnoreCase(tag.getCreatedBy())) {
+		if (! _tagsModel.getCreatedBy().equalsIgnoreCase(tag.getCreatedBy())) {
 			logger.warning("tag <" + id + ">: ignoring createdBy value <" + tag.getCreatedBy() +
 					"> because it was set on the client.");
 		}
-		_tag.setTitle(tag.getTitle());
-		_tag.setDescription(tag.getDescription());
-		_tag.setModifiedAt(new Date());
-		_tag.setModifiedBy(getPrincipal());
-		index.put(id, _tag);
-		logger.info("update(" + id + ") -> " + PrettyPrinter.prettyPrintAsJSON(_tag));
+		if (_tagsModel.getCounter() != tag.getCounter()) {
+			logger.warning("tag <" + id + ">: ignoring counter value <" + tag.getCounter() +
+					">because the counter can not be changed by the client.");
+		}
+		_tagsModel.setModifiedAt(new Date());
+		_tagsModel.setModifiedBy(getPrincipal());
+		_textedTag.setModel(_tagsModel);
+		index.put(id, _textedTag);
+		logger.info("update(" + id + ") -> " + PrettyPrinter.prettyPrintAsJSON(_tagsModel));
 		if (isPersistent) {
 			exportJson(index.values());
 		}
-		return _tag;
+		return _tagsModel;
 	}
 
 	/* (non-Javadoc)
@@ -198,19 +223,186 @@ public class FileServiceProvider implements AbstractFileServiceProvider<TagsMode
 	@Override
 	public void delete(
 		String id) 
-	throws NotFoundException. InternalServerErrorException {
-		TagsModel _tag = index.get(id);
-		if (_tag == null) {
-			throw new NotFoundException("tag <" + id
-					+ "> was not found.");
-		}
-		if (index.remove(id) == null) {
-			throw new InternalServerErrorException("tag <" + id
+	throws NotFoundException, InternalServerErrorException {
+		TextedTag _textedTag = readTextedTag(id);
+		TagsModel _tagsModel = _textedTag.getModel();
+		if (_tagsModel.getCounter() == 1) {	// remove the tag object from the index
+			if (index.remove(id) == null) {
+				throw new InternalServerErrorException("tag <" + id
 					+ "> can not be removed, because it does not exist in the index");
+			} else {			// remove was ok
+				logger.info("delete(" + id + ") -> removed from index.");
+			}
+		} else if (_tagsModel.getCounter() < 1) {
+				throw new InternalServerErrorException("tag <" + id + 
+						">: counter has invalid value <" + _tagsModel.getCounter() + "> (should be >= 1).");
 		}
-		logger.info("delete(" + id + ")");
+		else { // counter > 1 -> decrement it
+			_tagsModel.setCounter(_tagsModel.getCounter() - 1);
+			_textedTag.setModel(_tagsModel);
+			index.put(id, _textedTag);
+			logger.info("delete(" + id + ") -> decrement counter to <" + _tagsModel.getCounter() + ">.");
+		}
 		if (isPersistent) {
 			exportJson(index.values());
 		}
+	}
+
+	/************************************** localized texts (lang) ************************************/
+	@Override
+	public List<LocalizedTextModel> listTexts(
+			String tid, 
+			String queryType,
+			String query, 
+			int position, 
+			int size) {
+		List<LocalizedTextModel> _localizedTexts = readTextedTag(tid).getLocalizedTexts();
+		Collections.sort(_localizedTexts, LocalizedTextModel.LocalizedTextComparator);
+		
+		ArrayList<LocalizedTextModel> _selection = new ArrayList<LocalizedTextModel>();
+		for (int i = 0; i < _localizedTexts.size(); i++) {
+			if (i >= position && i < (position + size)) {
+				_selection.add(_localizedTexts.get(i));
+			}
+		}
+		logger.info("listTexts(<" + tid + ">, <" + query + ">, <" + queryType + 
+				">, <" + position + ">, <" + size + ">) -> " + _selection.size()
+				+ " values");
+		return _selection;
+	}
+
+	@Override
+	public LocalizedTextModel createText(
+			String tid, 
+			LocalizedTextModel tag)
+			throws DuplicateException, ValidationException {
+		if (tag.getText() == null || tag.getText().isEmpty()) {
+			throw new ValidationException("LocalizedText <" + tid + "/lang/" + tag.getId() + 
+					"> must contain a valid text.");
+		}
+		// enforce that the title is a single word
+		StringTokenizer _tokenizer = new StringTokenizer(tag.getText());
+		if (_tokenizer.countTokens() != 1) {
+			throw new ValidationException("LocalizedText <" + tid + "/lang/" + tag.getId() + 
+					"> must consist of exactly one word <" + tag.getText() + "> (is " + _tokenizer.countTokens() + ").");
+		}
+		TextedTag _textedTag = readTextedTag(tid);
+		if (tag.getLangCode() == null) {
+			throw new ValidationException("LocalizedText <" + tid + "/lang/" + tag.getId() + 
+					"> must contain a LanguageCode.");
+		}
+		if (_textedTag.containsLocalizedText(tag.getLangCode())) {
+			throw new DuplicateException("LocalizedText with LanguageCode <" + tag.getLangCode() + 
+					"> exists already in tag <" + tid + ">.");
+		}
+		String _id = tag.getId();
+		if (_id == null || _id.isEmpty()) {
+			_id = UUID.randomUUID().toString();
+		} else {
+			if (textIndex.get(_id) != null) {
+				throw new DuplicateException("LocalizedText with id <" + _id + 
+						"> exists alreday in index.");
+			}
+			else {
+				throw new ValidationException("LocalizedText <" + _id +
+						"> contains an ID generated on the client. This is not allowed.");
+			}
+		}
+
+		tag.setId(_id);
+		Date _date = new Date();
+		tag.setCreatedAt(_date);
+		tag.setCreatedBy(getPrincipal());
+		tag.setModifiedAt(_date);
+		tag.setModifiedBy(getPrincipal());
+		
+		textIndex.put(_id, tag);
+		_textedTag.addText(tag);
+		logger.info("createText(" + tid + "/lang/" + tag.getId() + ") -> " + PrettyPrinter.prettyPrintAsJSON(tag));
+		if (isPersistent) {
+			exportJson(index.values());
+		}
+		return tag;
+	}
+
+	@Override
+	public LocalizedTextModel readText(
+			String tid, 
+			String lid)
+			throws NotFoundException {
+		readTextedTag(tid);
+		LocalizedTextModel _localizedText = textIndex.get(lid);
+		if (_localizedText == null) {
+			throw new NotFoundException("LocalizedText <" + tid + "/lang/" + lid +
+					"> was not found.");
+		}
+		logger.info("readText(" + tid + "/lang/" + lid + ") -> "
+				+ PrettyPrinter.prettyPrintAsJSON(_localizedText));
+		return _localizedText;
+	}
+
+	@Override
+	public LocalizedTextModel updateText(
+			String tid, 
+			String lid,
+			LocalizedTextModel tag) 
+					throws NotFoundException, ValidationException {
+		readTextedTag(tid);
+		LocalizedTextModel _localizedText = textIndex.get(lid);
+		if (_localizedText == null) {
+			throw new NotFoundException("LocalizedText <" + tid + "/lang/" + lid +
+					"> was not found.");
+		}
+		if (! _localizedText.getCreatedAt().equals(tag.getCreatedAt())) {
+			logger.warning("LocalizedText <" + tid + "/lang/" + lid + ">: ignoring createAt value <" 
+					+ tag.getCreatedAt().toString() + "> because it was set on the client.");
+		}
+		if (! _localizedText.getCreatedBy().equalsIgnoreCase(tag.getCreatedBy())) {
+			logger.warning("LocalizedText <" + tid + "/lang/" + lid + ">: ignoring createBy value <"
+					+ tag.getCreatedBy() + "> because it was set on the client.");
+		}
+		if (_localizedText.getLangCode() != tag.getLangCode()) {
+			throw new ValidationException("LocalizedText <" + tid + "/lang/" + lid + 
+					">: it is not allowed to change the LanguageCode.");
+		}
+		_localizedText.setText(tag.getText());
+		_localizedText.setModifiedAt(new Date());
+		_localizedText.setModifiedBy(getPrincipal());
+		textIndex.put(lid, _localizedText);
+		logger.info("updateText(" + tid + ", " + lid + ") -> " + PrettyPrinter.prettyPrintAsJSON(_localizedText));
+		if (isPersistent) {
+			exportJson(index.values());
+		}
+		return _localizedText;
+	}
+
+	@Override
+	public void deleteText(
+			String tid, 
+			String lid) 
+					throws NotFoundException, InternalServerErrorException {
+		TextedTag _textedTag = readTextedTag(tid);
+		LocalizedTextModel _localizedText = textIndex.get(lid);
+		if (_localizedText == null) {
+			throw new NotFoundException("LocalizedText <" + tid + "/lang/" + lid +
+					"> was not found.");
+		}
+		
+		// 1) remove the LocalizedText from its TextedTag
+		if (_textedTag.removeText(_localizedText) == false) {
+			throw new InternalServerErrorException("LocalizedText <" + tid + "/lang/" + lid
+					+ "> can not be removed, because it is an orphan.");
+		}
+		// 2) remove the LocalizedText from the index
+		if (textIndex.remove(lid) == null) {
+			throw new InternalServerErrorException("LocalizedText <" + tid + "/lang/" + lid
+					+ "> can not be removed, because it does not exist in the index.");
+		}
+		
+			
+		logger.info("deleteText(" + tid + ", " + lid + ") -> OK");
+		if (isPersistent) {
+			exportJson(index.values());
+		}		
 	}
 }
